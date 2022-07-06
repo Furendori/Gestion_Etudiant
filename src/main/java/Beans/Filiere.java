8cd138c013d6f07fc2ac6f8c433733db302ef1e5
Delecroix Alexis <d-alexis@hotmail.fr>
2022-07-06 16:12:28 +0200

    Ariana love <3

--- src/main/java/Beans/Filiere.java
+++ src/main/java/Beans/Filiere.java
@@ -4,8 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Filiere {
+
+
+    Scanner sc = new Scanner(System.in);
+
+    public void CreateTableFiliere() {
+
+        try {
+            //étape 1: charger la classe driver
+            Class.forName("org.postgresql.Driver");
+            //étape 2: créer l'objet de connexion
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/Gestion_Etudiant", "postgres", "Kara59");
+            //étape 3: créer l'objet statement
+            Statement stmt = conn.createStatement();
+            //étape 4: exécuter la requéte
+            String sql = "CREATE TABLE Filiere2 " +
+                    "(numFiliere NUMERIC not NULL, " +
+                    " nomFiliere VARCHAR(50);";
+            stmt.executeUpdate(sql);
+            //étape 5: fermez l'objet de connexion
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 
     public void affichageFilière() {
         try {
@@ -16,7 +43,7 @@ public class Filiere {
                     "jdbc:postgresql://localhost:5432/Gestion_Etudiant", "postgres", "Kara59");
             //étape 3: créer l'objet statement
             Statement stmt = conn.createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM Filiere");
+            ResultSet res = stmt.executeQuery("SELECT * FROM Filiere2");
             //étape 4: exécuter la requête
             while (res.next())
                 System.out.println("L'id de la filière : "+res.getInt(1) +  " Le nom de la filière est :  "+ res.getString(2)
@@ -26,5 +53,66 @@ public class Filiere {
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public void AjoutFilière() {
+        try {
+            //étape 1: charger la classe driver
+            Class.forName("org.postgresql.Driver");
+            //étape 2: créer l'objet de connexion
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/Gestion_Etudiant", "postgres", "Kara59");
+            //étape 3: créer l'objet statement
+            Statement stmt = conn.createStatement();
+            //étape 4: exécuter la requête
+            System.out.println("Insertion...");
+            String sql = "INSERT INTO Filiere2 " +
+                    "VALUES (1 , 'Electronique')";
+            stmt.executeUpdate(sql);
+             sql = "INSERT INTO Filière2 " +
+                    "VALUES (2 , 'Informatique')";
+            stmt.executeUpdate(sql);
+            System.out.println("Données insérés dans la table...");
+            //étape 5: fermez l'objet de connexion
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+    public void AjoutFilièretSaisie(){
+        try {
+            //étape 1: charger la classe driver
+            Class.forName("org.postgresql.Driver");
+            //étape 2: créer l'objet de connexion
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/Gestion_Etudiant", "postgres", "Kara59");
+            //étape 3: créer l'objet statement
+            Statement stmt = conn.createStatement();
+            //étape 4: exécuter la requête
+
+            int id_Filiere;
+            String nomFiliere;
+
+            System.out.println("Choisissez un id");
+            id_Filiere = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Choissisez un nom pour la filière");
+            nomFiliere = sc.nextLine();
+
+
+            String sql = "INSERT INTO Filière2 " +
+                    "VALUES ("+id_Filiere+"  , '"+nomFiliere+"')";
+            stmt.executeUpdate(sql);
+
+
+            System.out.println("Données insérés dans la table...");
+            //étape 5: fermez l'objet de connexion
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
---
+++ src/main/java/Beans/Menu.java
@@ -0,0 +1,67 @@
+package Beans;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    Etudiant etude = new Etudiant();
+    Filiere filiere = new Filiere();
+    public void Menu() {
+        int choix = 0;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("**********************************************************************************************");
+            System.out.println("*                  Bonjour,                                                                  *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 1 :  Création des tables étudiants et filières.                                     *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 2 :  Ajouts de 8 étudiants, et de 2 filières.                                       *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 3 :  Affichage des étudiants, et des filières.                                      *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 4 :  Ajouts d'un nouvelle étudiants en entrant ses informations.                    *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 5 :  Ajouts d'une nouvelle filière en entrant ses informations                      *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 6 :  Suppression d'un étudiant                                                      *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 7 :  Suppression d'une filière                                                      *");
+            System.out.println("*                                                                                            *");
+            System.out.println("*  Tapez 8 : Quitter le programme                                                            *");
+            System.out.println("*                                                                                            *");
+            System.out.println("**********************************************************************************************");
+            System.out.println("Entre votre choix ?");
+            choix = sc.nextInt();
+            switch (choix) {
+                case 1:
+                    etude.CreateTable();
+                    filiere.CreateTableFiliere();
+                    break;
+                case 2:
+                    etude.AjoutEtudiant();
+                    filiere.AjoutFilière();
+                    break;
+                case 3:
+                    etude.affichageEtudiant();
+                    filiere.affichageFilière();
+                    break;
+                case 4:
+                    etude.AjoutEtudiantSaisie();
+                    break;
+                case 5:
+                    filiere.affichageFilière();
+                    break;
+                case 6:
+
+                case 7:
+
+            }
+        } while(choix != 8);
+
+
+
+
+
+    }
+}
